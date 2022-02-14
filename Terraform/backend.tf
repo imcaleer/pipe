5186c01ef95a2d6f11407dfa4c396edb5ec9a53f
imcaleer <imcaleer@deloitte.co.uk>
2022-02-14 09:59:35 +0000

    commit

--- Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ct-sandbox-imca"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-lock"
-
-  }
-}
