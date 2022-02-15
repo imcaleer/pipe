bb27146afdd8409c4ca05ccce0f2a85d70324daa
imcaleer <imcaleer@deloitte.co.uk>
2022-02-15 13:24:53 +0000

    changed lambda

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -16,13 +16,32 @@ variable "function_name" { default = "mytestlambda" }
 variable "handler" { default = "lambda.lambda_handler" }
 variable "runtime" { default = "python3.8" }
 
+data "archive_file" "zipit" {
+  type        = "zip"
+  source_file = "${path.module}/lambda.py"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "uploads-imca-lambda"
+}
+
+resource "aws_s3_bucket_object" "zipit" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "lambda.zip"
+  source = data.archive_file.zipit.output_path
+
+  etag = filemd5(data.archive_file.zipit.output_path)
+}
+
 resource "aws_lambda_function" "lambda_function" {
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = var.handler
-  runtime          = var.runtime
-  filename         = "lambda.zip"
-  function_name    = var.function_name
-  source_code_hash = filebase64sha256("lambda.zip")
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  function_name = var.function_name
+  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
+  s3_key        = "lambda.zip"
 
   depends_on = [
     aws_iam_role_policy_attachment.lambda_CloudWatchLogs
@@ -30,7 +49,7 @@ resource "aws_lambda_function" "lambda_function" {
 }
 
 resource "aws_s3_bucket" "upload_bucket" {
-  bucket = "uploads-imca"
+  bucket = "uploads-imca2"
 }
 
 resource "aws_s3_bucket_notification" "my-trigger" {
@@ -62,7 +81,7 @@ resource "aws_lambda_permission" "test" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.arn
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::uploads-imca"
+  source_arn    = "arn:aws:s3:::uploads-imca2"
 }
 
 
