d4dbd008d6f230798cd28defc9171010fb989909
imcaleer <imcaleer@deloitte.co.uk>
2022-02-20 20:24:48 +0000

    changes

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -16,59 +16,6 @@ variable "function_name" { default = "mytestlambda" }
 variable "handler" { default = "lambda.lambda_handler" }
 variable "runtime" { default = "python3.8" }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "uploads-imca-lambda-2"
-}
-
-data "archive_file" "zipit" {
-  type        = "zip"
-  source_file = "${path.module}/lambda.py"
-  output_path = "${path.module}/lambda.zip"
-}
-
-
-resource "aws_s3_bucket_object" "zipit" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  key    = "lambda.zip"
-  source = data.archive_file.zipit.output_path
-
-  etag = filemd5(data.archive_file.zipit.output_path)
-
-
-}
-
-resource "aws_lambda_function" "lambda_function" {
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.handler
-  runtime       = var.runtime
-  function_name = var.function_name
-  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
-  s3_key        = "lambda.zip"
-
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_CloudWatchLogs
-  ]
-}
-
-resource "aws_s3_bucket" "upload_bucket" {
-  bucket = "imca-source-2"
-}
-
-resource "aws_s3_bucket_notification" "my-trigger" {
-  bucket = aws_s3_bucket.upload_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_function.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_suffix       = ".json"
-  }
-
-  depends_on = [
-    aws_lambda_permission.test
-  ]
-}
-
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "employees"
   hash_key       = "emp_id"
@@ -83,21 +30,43 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
 
 }
 
+
+
+resource "aws_lambda_function" "lambda_function" {
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = var.handler
+  runtime          = var.runtime
+  filename         = "lambda.zip"
+  function_name    = var.function_name
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_CloudWatchLogs
+  ]
+}
+
+resource "aws_s3_bucket" "upload_bucket" {
+  bucket = "uploads-imca"
+}
+
+resource "aws_s3_bucket_notification" "my-trigger" {
+  bucket = aws_s3_bucket.upload_bucket.bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".json"
+  }
+}
+
 resource "aws_lambda_permission" "test" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
+  function_name = aws_lambda_function.lambda_function.arn
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.upload_bucket.arn
-  qualifier     = aws_lambda_alias.test_alias.name
 }
 
-resource "aws_lambda_alias" "test_alias" {
-  name             = "testalias"
-  description      = "a sample description"
-  function_name    = aws_lambda_function.lambda_function.function_name
-  function_version = "$LATEST"
-}
 
 
 resource "aws_iam_role" "iam_for_lambda" {
